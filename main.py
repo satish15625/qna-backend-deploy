4656a2ccb5c33b56eeb2ede5343ca500fd93480f
Satish kumar <satish15625@gmail.com>
2025-06-20 23:42:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ def index():
     return {"message": "Welcome to the RAG Pipeline API!"}
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
